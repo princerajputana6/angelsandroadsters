55475e9d27a9fd9325491a5ce40158fb49af30fd
Prince kumar <princerajputana6@gmail.com>
2026-06-17 02:10:17 +0530

    0

---
+++ src/app/head.jsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        src="https://www.trylinqr.com/widget.js"
+        data-org-id="6a1957b3c40c498c73bfe0eb"
+        async
+      ></script>
+    </>
+  );
+}
